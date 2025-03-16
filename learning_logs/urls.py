fd163d5efc2e3ceffe8f5a7be294447217095f75
HuyDD305 <bribribridu@gmail.com>
2025-03-16 10:09:43 +0700

    change admin, change base, add topic, topics and entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -1,10 +1,17 @@
 """Defind URL pattens for learning_logs"""
 
 from django.urls import path
+# import the path function, which is needed
+# when mapping URLs to views
 from . import views
 
-app_name = 'learning_logs'
+app_name = 'learning_logs' # Django automatically detects this!
 urlpatterns = [
     # Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    # Page that shows all topic
+    path('topics/', views.topics, name='topics'),
+    # Details page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
+
