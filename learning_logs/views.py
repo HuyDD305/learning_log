69765a37c8aa3b07f27c07324f5315d4afea8bb6
HuyDD305 <bribribridu@gmail.com>
2025-03-28 14:01:41 +0700

    add new_topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import TopicFrom
 from .models import Topic
 
 # Create your views here.
@@ -20,4 +22,17 @@ def topic(request, topic_id):
     return render(request, 'learning_logs/topic.html', context)
 
 def new_topic(request):
-    pass
+    """Add a new topic for user"""
+    if request.method != 'POST':
+        #No data submitted; create a blank form.
+        form = TopicFrom()
+    else:
+        #POST data submitted; process data.
+        form = TopicFrom(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs/topics')
+
+    #Display a blank or invalid form.
+    context = {'from': form}
+    return render(request, 'learning_logs/new_topic.html', context)
