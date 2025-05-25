d0c0bd501547a7756ace19ae9aa192da260e3479
HuyDD305 <bribribridu@gmail.com>
2025-05-26 00:50:51 +0700

    DONEEEEE

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 from django.conf.global_settings import LOGIN_URL
@@ -135,6 +135,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #My setting
 LOGIN_URL = 'users:login'
 
-#Heroku settings.
-import django_heroku
-django_heroku.settings(locals())
+
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
